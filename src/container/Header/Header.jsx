a346b430dafc013b8dc53a0dbbc6bfe2feb107be
VJS <minsung_96@naver.com>
2022-02-24 23:59:05 +0900

    About #1

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -11,9 +11,18 @@ const Header = () => (
       <h1 className="app__header-h1">
         The Key to Fine Dining
       </h1>
+      <p className="p__opensans" style={{ margin: '2rem 0'}}>
+        Lorem Ipsum
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
     <div className="app__wrapper_img">
-
+      <img 
+        src={images.welcome}
+        alt="header image"
+      />
     </div>
   </div>
 );
