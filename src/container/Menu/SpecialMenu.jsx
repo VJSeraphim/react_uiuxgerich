b987f408d98b2bd345eb36f1c0d535cfc1b59a45
VJS <minsung_96@naver.com>
2022-02-25 23:56:54 +0900

    Menu Component #1

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { images, data } from '../../constants'
+import { SubHeading, MenuItem } from '../../components'
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu for you" />
+      <h1 className="headtext__cormorant">
+        Today's Special Cuisine
+      </h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      
+    </div>
   </div>
 );
 
