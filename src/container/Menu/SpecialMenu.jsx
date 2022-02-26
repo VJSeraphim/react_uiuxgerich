af7dd9f86ecddb7e3e881f1562f92a26b5162037
VJS <minsung_96@naver.com>
2022-02-26 23:56:59 +0900

    Menu #2

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { images, data } from '../../constants'
 import { SubHeading, MenuItem } from '../../components'
 import './SpecialMenu.css';
+import { GiWineBottle } from 'react-icons/gi';
 
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
@@ -13,7 +14,39 @@ const SpecialMenu = () => (
       </h1>
     </div>
     <div className="app__specialMenu-menu">
-      
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu_menu_heading">
+          Wine & Beer
+        </p>
+        <div className="app__specialMenu_menu_items">
+          {data.wines.map((wine, i) => (
+            <MenuItem key={wine.title + i} title={wine.price} tags={wine.tags}/>
+          ))}
+        </div>
+      </div>
+      <div className="app__specialMenu-menu_img">
+        <img 
+          src={images.menu}
+          alt="menu image"
+        />
+      </div>
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu_menu_heading">
+          Wine & Beer
+        </p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktail, i) => (
+            <p>
+              <MenuItem key={cocktail.title + i} title={cocktail.price} tags={cocktail.tags}/>
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
+    <div stype={{ marginTop : '15px' }}>
+      <button type="button" className="custom__button">
+        View More
+      </button>
     </div>
   </div>
 );
