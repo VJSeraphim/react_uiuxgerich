433dddd9c8d6844ec50069ccdfcf87b1449cc2e3
VJS <minsung_96@naver.com>
2022-03-03 23:58:30 +0900

    Gallery Section #1

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
 
 import './Gallery.css';
+import { SubHeading } from '../../components'
+import { images, data } from '../../constants'
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  const scrollRef = useRef(null)
+
+  const scroll = (dir) => {
+    const { current } = scrollRef
+    if (dir === 'left') {
+      current.scrollLeft -= 300 
+    } else {
+      current.scrollLeft += 300
+    }
+  }
+
+  const galImages = [
+    images.gallery01,
+    images.gallery02,
+    images.gallery03,
+    images.gallery04
+  ]
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram"/>
+        <h1 className="headtext__cormorant">
+          Gallery
+        </h1>
+        <p className="p__opensans" style={{ color:'#AAA', marginTop: '2rem' }}>
+          Lorem Ipsum
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+      <div className="app__gallery-images">
+        <div ref={scrollRef} className="app__gallery-images_container">
+          {galImages.map((image, i) => {
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${i + 1}`}>
+              <img 
+                src={image}
+                alt="gallery"
+              />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          })}
+        </div>
+        <div className="app__gallery-images_arrow">
+          <BsArrowLeftShort className="galler__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="galler__arrow-icon" onClick={() => scroll('right')} />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Gallery;
